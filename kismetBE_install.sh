2d744b411e078e496560db0a441f3805b61a9c32
cyberintelpro <cyberintelpro@gmail.com>
2017-11-04 11:32:58 -0400

    install script
    this script is tested and works to install kismet on a debian based device

---
+++ kismetBE_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# A script to install kismet bleeding edge
+
+sudo apt-get update \
+  && sudo apt-get upgrade -y \
+  && sudo apt-get install -y \
+    gpsd \
+    gpsd-clients \
+    build-essential \
+    libmicrohttpd-dev \
+    git \
+    libnl-3-dev \
+    libnl-genl-3-dev \
+    libcap-dev \
+    libpcap-dev \
+    libncurses5-dev \
+    libnm-dev \
+    libdw-dev \
+    libsqlite3-dev \
+    && sudo apt-get remove -y wpasupplicant \
+    && cd /home/pi/ \
+    && sudo wget -O manuf 'https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf' \
+    && sudo cp manuf /etc/. \
+    && sudo rm manuf \
+    && git clone https://www.kismetwireless.net/git/kismet.git \
+    && cd /home/pi/kismet \
+    && sudo ./configure \
+    && sudo make -j2 \
+    && sudo make suidinstall \
+    && sudo usermod -a -G kismet pi \
+    && sudo reboot
