1d99eefecf2e0e3e96a3eb81dac87999e710ec93
cyberintelpro <cyberintelpro@gmail.com>
2017-11-04 16:38:01 -0400

    fixed bash issues

--- config_kismet.sh
+++ config_kismet.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 # A script to configure kismet bleeding edge
 
+read -r -p "Do you want to set the kismet web UI username and password? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  if grep -v '^#' /usr/local/etc/kismet_httpd.conf | grep -v '^$'| grep -q  'httpd_username=*'
 
-&& read -p 'Set Kismet Username: ' uservar \
-&& read -sp 'Set Kismet Password: ' passwd \
-&& sudo sh -c "echo 'httpd_username=$uservar' >> /usr/local/etc/kismet_httpd.conf" \
-&& sudo sh -c "echo 'httpd_password=$passwd' >> /usr/local/etc/kismet_httpd.conf" \
-&& iwconfig 2>/dev/null | grep -o "^\w*" \
-&& read -p 'Which wireless interface do you want to use? ' winterface \
-&& read -p 'What do you want to name the selected wireless? ' winame \
-&& sudo sh -c "echo 'source=$winterface:name=$winame' >> /usr/local/etc/kismet.conf" \
-&& hciconfig  2>/dev/null | grep -o "^\w*"
+  then
+      print 'Username is already set'
+      grep -v '^#' /usr/local/etc/kismet_httpd.conf | grep -v '^$'| grep 'httpd_username=*'
+  else
+      read -p 'Set Kismet Username: ' uservar
+      sudo sh -c "echo 'httpd_username=$uservar' >> /usr/local/etc/kismet_httpd.conf"
+  fi
+  if grep -v '^#' /usr/local/etc/kismet_httpd.conf | grep -v '^$'| grep -q 'httpd_password=*'
+
+  then
+      print 'Password is already set'
+      grep -v '^#' /usr/local/etc/kismet_httpd.conf | grep -v '^$'| grep  'httpd_password=*'
+  else
+      read -sp 'Set Kismet Password: ' userpass
+      sudo sh -c "echo 'httpd_password=$userpass' >> /usr/local/etc/kismet_httpd.conf"
+  fi
+fi
+
+read -r -p "Do you want to set a wireless collection interface? [y/N] " responsew
+if [[ "$responsew" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  iwconfig 2>/dev/null | grep -o "^\w*"
+  read -p 'Which wireless interface do you want to use? ' winterface
+  read -p 'What do you want to name the selected wireless? ' winame
+  sudo sh -c "echo 'source=$winterface:name=$winame' >> /usr/local/etc/kismet.conf"
+fi
+
+read -r -p "Do you want to set a bluetooth collection interface? [y/N] " responsebt
+if [[ "$responsebt" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  hciconfig  2>/dev/null | grep -o "^\w*"
+  read -p 'Which bluetooth interface do you want to use? ' btinterface
+  read -p 'What do you want to name the selected wireless? ' btname
+  sudo sh -c "echo 'source=$btinterface:name=$btname' >> /usr/local/etc/kismet.conf"
+fi
